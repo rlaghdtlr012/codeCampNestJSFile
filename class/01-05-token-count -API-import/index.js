ec06cc8f58896a131e07c32dcc1f03c7da4211a3
unknown <rlaghdtlr012@naver.com>
2023-02-12 23:51:25 +0900

    first commit

---
+++ class/01-05-token-count -API-import/index.js
@@ -0,0 +1,10 @@
+import { checkValidationPhone, getToken, sendTokenToSMS} from "./phone.js";
+function createTokenOfPhone(myPhone, count) {
+  const isValid = checkValidationPhone(myPhone);
+  if(isValid) {
+    const myToken = getToken(6); 
+    sendTokenToSMS(myPhone, myToken)
+  }
+}
+
+createTokenOfPhone("01026373885", 6);
